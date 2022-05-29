a85c9587cbc19544397e14b9454055a0d8abb554
no382001 <102482527+no382001@users.noreply.github.com>
2022-05-29 15:32:19 +0200

    added selectable pre processing options in the form of scales and dropdowns

--- gui.py
+++ gui.py
@@ -13,11 +13,48 @@ wheight = 600
 wwidth = 600
 dialogmargin = 50
 pages = []
+shown_image = []
 current_index = 0
 
 preproc = False
 
+kernel_options = ["cv.MORPH_CROSS","cv.MORPH_OPEN","cv.MORPH_CLOSE","cv.MORPH_GRADIENT","cv.MORPH_TOPHAT","cv.MORPH_BLACKHAT"]
+
+cv_kernel = cv.getStructuringElement(eval(kernel_options[0]),(3,3))
+        
+adaptive_options = ["cv.ADAPTIVE_THRESH_MEAN_C","cv.ADAPTIVE_THRESH_GAUSSIAN_C"]
+
+cv_adaptive = eval(adaptive_options[0])
+
+thresh_options = ["cv.THRESH_BINARY_INV","cv.THRESH_BINARY"]
+
+cv_thresh =  eval(thresh_options[0])
+
+allowed_difference = 30
+
+dilate_no_iteration = 2
+
 class App(tk.Frame):
+    
+    def preProcess(self):
+        global pages, current_index, cv_kernel, shown_image
+
+        #converting PIL to np array
+        nparray = np.array(pages[current_index])
+        #inverting RGB to BGR for opencv compatibility
+        nparray = nparray[:, :, ::-1].copy()
+        #BGR to Grayscale
+        open_cv_image = cv.cvtColor(nparray, cv.COLOR_BGR2GRAY)
+        #applying threshold, making the image 2bits in color, and inverting the image
+        open_cv_image = cv.adaptiveThreshold(open_cv_image, 255,cv_adaptive,cv_thresh,9,11)
+        #dilating, to smudge visible elements, essentially normalizing the image
+        open_cv_image = cv.dilate(open_cv_image,cv_kernel,iterations = dilate_no_iteration)
+        
+        return open_cv_image
+
+        #resizing image to a lesser size for time efficiency
+        #open_cv_image = resizeCvImage(open_cv_image)
+
     def __init__(self, parent): 
         tk.Frame.__init__(self, parent) #parent is the main canvas
 
@@ -36,14 +73,50 @@ class App(tk.Frame):
         rightbutton.configure(width = 5, activebackground = "#33B5E5")
         rightbutton_window = self.canvas.create_window(((wwidth/2)+100,wheight-45), window=rightbutton)
 
-        applypreproc = tk.Button(parent, text="toggle preproc",command=self)
+        applypreproc = tk.Button(parent, text="toggle preproc",command=self.togglePP)
         applypreproc.configure(width = 10, activebackground = "#33B5E5")
-        applypreproc_window = self.canvas.create_window(((wwidth/2),wheight-80), window=applypreproc)
+        applypreproc = self.canvas.create_window(((wwidth/2),wheight-80), window=applypreproc)
+
+
+        dilate_scale =  tk.Scale(parent,from_= 0,to = 100, orient='horizontal', command=self.dilateScaleChanged)
+        dilate_scale.set(dilate_no_iteration)
+        dilate_scale.configure(width = 10, activebackground = "#33B5E5")
+        dilate_scale_window = self.canvas.create_window(((wwidth/2),wheight-150), window=dilate_scale)
+
+        
+        global kernel_options
+        default_kernel = tk.StringVar()
+        default_kernel.set(kernel_options[0])
+
+        kernel_dropdown = tk.OptionMenu(parent, default_kernel,*kernel_options,command=self.kernelSelected)
+        kernel_dropdown.configure(width = 20, activebackground = "#33B5E5")
+        kernel_dropdown_window = self.canvas.create_window(((wwidth/2),wheight-200), window=kernel_dropdown)
+
+        global adaptive_options
+        default_adaptive = tk.StringVar()
+        default_adaptive.set(adaptive_options[0])
+
+        adaptive_dropdown = tk.OptionMenu(parent, default_adaptive,*adaptive_options,command=self.adaptiveSelected)
+        adaptive_dropdown.configure(width = 20, activebackground = "#33B5E5")
+        adaptive_dropdown_window = self.canvas.create_window(((wwidth/2),wheight-220), window=adaptive_dropdown)
+
+        global thresh_options
+        default_thresh = tk.StringVar()
+        default_thresh.set(thresh_options[0])
+
+        thresh_dropdown = tk.OptionMenu(parent, default_thresh,*thresh_options,command=self.threshSelected)
+        thresh_dropdown.configure(width = 20, activebackground = "#33B5E5")
+        thresh_dropdown_window = self.canvas.create_window(((wwidth/2),wheight-240), window=thresh_dropdown)
 
     def drawImage(self):
-        global current_index
+        global current_index, shown_image
         self.canvas.delete("image")
-        self.img = pages[current_index].resize((wwidth-20, wheight-20), Image.ANTIALIAS)
+
+        if not preproc:
+            self.img = pages[current_index].resize((wwidth-20, wheight-20))
+        else:
+            self.img = Image.fromarray(self.preProcess()).resize((wwidth-20, wheight-20))
+
         self.img = ImageTk.PhotoImage(self.img)
         self.canvas.create_image(wwidth/2,(wheight/2)+100,image=self.img,tags="image") #(wheight/2)+100 displacement so there is space for the selection and transformation of the rectangle 
     
@@ -54,12 +127,13 @@ class App(tk.Frame):
         
         for page_num in range(start, end):
             pdf_writer.addPage(pdf_reader.getPage(page_num))
-        
+
         cut_filename = f'cut.pdf'
         with open(cut_filename,'wb') as out:
             pdf_writer.write(out)
 
         pages = convert_from_path(cut_filename)
+        os.remove("cut.pdf")#                       use /tmp instead
         print("pages loaded",len(pages))
 
     def openFileDialog(self):
@@ -89,6 +163,34 @@ class App(tk.Frame):
         global current_index
         current_index += 1
         self.drawImage()
+
+    def togglePP(self):
+        global preproc
+        preproc = not preproc
+        print("preprocess is",preproc)
+        self.drawImage()
+
+    def dilateScaleChanged(self,passed):
+        global dilate_no_iteration
+        dilate_no_iteration = int(passed)
+        self.drawImage()
+
+    def kernelSelected(self,passed):
+        global cv_kernel
+        cv_kernel = eval(passed)
+        self.drawImage()
+
+    def adaptiveSelected(self,passed):
+        global cv_adaptive
+        cv_adaptive = eval(passed)
+        self.drawImage()
+
+    def threshSelected(self,passed):
+        global cv_thresh
+        cv_thresh = eval(passed)
+        self.drawImage()
+        
+
         
 
 if __name__ == "__main__":
